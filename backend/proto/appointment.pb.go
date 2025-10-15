2ec6429843b6fe2e728054ea079920f20cd1cbed
Hammed Oyedele <itz.harmid@gmail.com>
2025-10-15 23:11:15 +0100

    fix(proto): update Go package path in proto and generated files

--- backend/proto/appointment.pb.go
+++ backend/proto/appointment.pb.go
@@ -387,7 +387,7 @@ const file_proto_appointment_proto_rawDesc = "" +
 	"\x12AppointmentService\x12\\\n" +
 	"\x11CreateAppointment\x12\".schedule.CreateAppointmentRequest\x1a#.schedule.CreateAppointmentResponse\x12Y\n" +
 	"\x10ListAppointments\x12!.schedule.ListAppointmentsRequest\x1a\".schedule.ListAppointmentsResponse\x12\\\n" +
-	"\x11DeleteAppointment\x12\".schedule.DeleteAppointmentRequest\x1a#.schedule.DeleteAppointmentResponseB\x13Z\x11/proto;schedulepbb\x06proto3"
+	"\x11DeleteAppointment\x12\".schedule.DeleteAppointmentRequest\x1a#.schedule.DeleteAppointmentResponseB\x14Z\x12./proto;schedulepbb\x06proto3"
 
 var (
 	file_proto_appointment_proto_rawDescOnce sync.Once
